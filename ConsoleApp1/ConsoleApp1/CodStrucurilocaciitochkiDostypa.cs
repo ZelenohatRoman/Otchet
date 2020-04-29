8aafc4b34703e9bed24ed5718cae982884c917df
ZelenohatRoman <49244478+ZelenohatRoman@users.noreply.github.com>
2020-04-29 15:03:44 +0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/CodStrucurilocaciitochkiDostypa.cs
@@ -0,0 +1,14 @@
+﻿namespace MoscowSport.core
+{
+    public struct Location
+    {
+        public Location(double longitude, double latitude)
+        {
+            Longitude = longitude;
+            Latitude = latitude;
+        }
+
+        public double Longitude {get;}
+        public double Latitude {get;}
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Laba1.cs
@@ -0,0 +1,13 @@
+﻿namespace MoscowSport.core
+{
+    public class Locations
+    {
+        public string Fullname { get; set; }
+        public string Legaladdress { get; set; }
+        public string Headposition { get; set; }
+        public string FIOposition { get; set; }
+        public int INN { get; set; }
+        public int OGRN { get; set; }
+
+    }
+}
